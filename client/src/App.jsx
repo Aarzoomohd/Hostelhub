fb8fc40ccd9e9c1967b154624d792c88760befbd
Aarzoomohd <aarzoomohammad5@gmail.com>
2026-09-06 18:37:58 +0530

    Add protected route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,33 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import ProtectedRoute from "./components/ProtectedRoute.jsx"; // apna sahi path daalo
 
-import MainPage from "./pages/MainPage.jsx";
-import Login from "./pages/auth/Login.jsx";
-import Register from "./pages/auth/register.jsx";
+import MainPage from "./pages/MainPage";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/register";
 
-import Dashboard from "./pages/Dashboard.jsx";
-import Students from "./pages/Students.jsx";
-import Rooms from "./pages/Rooms.jsx";
+import Dashboard from "./pages/Dashboard";
+import Students from "./pages/Students";
+import Rooms from "./pages/Rooms";
 import Payments from "./pages/Payment.jsx";
-import HostelDetails from "./pages/HostelDetails.jsx";
+import HostelDetails from "./pages/HostelDetails";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
 
 const App = () => {
   return (
     <Routes>
       {/* FIRST PAGE */}
-      <Route path="/" element={<MainPage />} />
+      <Route path="/" element={<MainPage />} /> 
 
       {/* AUTH */}
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
 
-      {/* PROTECTED APP ROUTES */}
-      <Route element={<ProtectedRoute />}>
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/dashboard/students" element={<Students />} />
-        <Route path="/api/room" element={<Rooms />} />
-        <Route path="/api/payment" element={<Payments />} />
-        <Route path="/api/hostel" element={<HostelDetails />} />
+      {/* APP */}
+      <Route element= {<ProtectedRoute/>} >
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/dashboard/students" element={<Students />} />
+      <Route path="/api/room" element={<Rooms />} />
+      <Route path="/api/payment" element={<Payments />} />
+      <Route path="/api/hostel" element={<HostelDetails />} />
       </Route>
 
       {/* INVALID URL */}
