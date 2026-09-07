6888f6623be52336bc5dc33d7563de48796bbf31
Aarzoomohd <aarzoomohammad5@gmail.com>
2026-09-07 18:23:34 +0530

    Fix SMTP connection timeout

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -14,17 +14,15 @@ import PasswordReset from "../models/PasswordReset.js";
 // ===============================
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-});;
-
+});
 
 // ===============================
 // SEND EMAIL VERIFICATION OTP
